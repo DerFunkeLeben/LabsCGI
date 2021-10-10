5998f024fb8a9cec7b1479a7186d6fc559c28fb7
Егор Солонин <e.solonin@onpoint.ru>
2021-10-10 23:43:40 +0300

    started lab5

--- question_6.py
+++ question_6.py
@@ -1,43 +1,53 @@
-from pyglet.gl import *
 import pyglet
-from pyglet import app, graphics
+from pyglet import app, gl, graphics
 from pyglet.window import Window, key
 import numpy as np
-d, d1, d2 = 5, 10, 15
-wx, wy = 1.5 * d2, 1.5 * d2
-width, height = int(30 * wx), int(30 * wy)
-window = Window(visible = True, width = width, height = height, resizable = True)
-glClearColor(0.4, 0.4, 0.4, 1.0)
-glClear(GL_COLOR_BUFFER_BIT)
-def texInit():
-    iWidth = iHeight = 64
-    n = 3 * iWidth * iHeight
-    img = np.zeros((iWidth, iHeight, 3), dtype = 'uint8')
-    for i in range(iHeight):
-     for j in range(iWidth):
-        img[i, j, :] = (i & 16 ^ j & 16) * 255
-    img = img.reshape(n)
-    img = (GLubyte * n)(*img)
-    p, r = GL_TEXTURE_2D, GL_RGB
-    glTexParameterf(p, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(p, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameterf(p, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameterf(p, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-    glTexImage2D(p, 0, r, iWidth, iHeight, 0, r, GL_UNSIGNED_BYTE, img)
-    glEnable(p)
-
-texInit()
-
-
-zv = -d2/2
-v0, v1, v2, v3 = (-d2,d2,zv), (-d1,d1,0), (d1,d1,0), (d2,d2,zv)
+from sys import exit
+d = 12
+wx, wy = 1.5 * d, 1.1 * d # Параметры области визуализации
+width, height = int(20 * wx), int(20 * wy) # Размеры окна вывода
+#
+mtClr0 = [1, 1, 0, 0] # Цвет материала
+light_position0 = [0, 40, 40, 0] # Позиция источника света
+lghtClr0 = [0.75, 0, 0, 0] # Цвет источника света
+mtClr = (gl.GLfloat * 4)()
+light_position = (gl.GLfloat * 4)()
+lghtClr = (gl.GLfloat * 4)()
+for k in range(4): mtClr[k] = mtClr0[k]
+for k in range(4): light_position[k] = light_position0[k]
+for k in range(4): lghtClr[k] = lghtClr0[k]
+#
+window = Window(visible = True, width = width, height = height,
+                resizable = True, caption = 'Материал')
+gl.glClearColor(0.1, 0.1, 0.1, 1.0)
+gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+gl.glEnable(gl.GL_LIGHTING) # Активизируем использование материалов
 @window.event
 def on_draw():
     window.clear()
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-wx, wx, -wy, wy, -20, 20)
-    glRotatef(60, 1, 0, 0)
-    graphics.draw(4, GL_QUADS, ('v3f', (v0 + v1 + v2 + v3)), ('t2f', (0,1, 0,0, 1,0, 1,1)))
+    gl.glMatrixMode(gl.GL_PROJECTION)
+    gl.glLoadIdentity()
+    gl.glOrtho(-wx, wx, -wy, wy, -1, 1)
+    gl.glShadeModel(gl.GL_SMOOTH) # GL_FLAT - без интерполяции цветов
+    gl.glMaterialfv(gl.GL_FRONT, gl.GL_SPECULAR, mtClr)
+    gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, light_position)
+    gl.glLightfv(gl.GL_LIGHT0, gl.GL_SPECULAR, lghtClr)
+    gl.glEnable(gl.GL_LIGHT0) # Включаем в уравнение освещенности источник GL_LIGHT0
+    gl.glColor3f(1, 0, 0)
+    gl.glBegin(gl.GL_QUADS)
+    gl.glNormal3f(0, 0, 1)
+    gl.glVertex3f(-d, -d, 0)
+    gl.glNormal3f(0, 0, 1)
+    gl.glVertex3f(d, -d, 0)
+    gl.glNormal3f(0, 0, 1)
+    gl.glVertex3f(d, d, 0)
+    gl.glNormal3f(0, 0, -1)
+    gl.glVertex3f(-d, d, 0)
+    gl.glEnd()
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key._1:
+        gl.glDisable(gl.GL_LIGHTING)
+    elif symbol == key._2:
+        gl.glEnable(gl.GL_LIGHTING)
 app.run()
---
+++ А14_СолонинЕгор_ЛР5.py
@@ -0,0 +1,241 @@
+from pyglet.gl import *
+import pyglet
+from pyglet import app, graphics, clock
+from pyglet.window import Window, key
+import numpy as np
+
+width, height = 1000, 1000
+w = 10
+window = Window(visible=True, width=width, height=height, resizable=True)
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+glClearColor(0.4, 1.0, 0.8, 1.0)
+glLineWidth(3)
+
+settings = {
+    'display_bases': True,
+    'display_normals': True,
+    'depth_test': True,
+    'cull_face': True,
+    'static_source_active': True,
+    'lightning_active': True,
+    'normalize_manual': True,
+    'normalize_auto': False,
+    'diffuse': True,
+    }
+
+
+
+def show_axis():
+    glBegin(GL_LINES)
+    glColor3f(0, 0, 0)
+
+    glVertex3f(0, 0, 0)
+    glVertex3f(w, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, w, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, w)
+
+    glEnd()
+
+    glEnable(GL_LINE_STIPPLE)
+    glLineStipple(1, 0x1C47)
+    glLineWidth(2)
+
+    glBegin(GL_LINES)
+
+    glVertex3f(-w, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, -w, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, -w)
+    glVertex3f(0, 0, 0)
+
+    glEnd()
+    glDisable(GL_LINE_STIPPLE)
+
+
+def get_normals_lateral(coords, l):
+    normals = []
+
+    for i in range(len(coords)):
+        bl = [coords[i][0], coords[i][1], coords[i][2]]
+        tl = [coords[i][0], coords[i][1] + 2 * l, coords[i][2]]
+        tr = [coords[0][0], coords[0][1] + 2 * l, coords[0][2]] if i == len(
+            coords)-1 else [coords[i+1][0], coords[i+1][1] + 2 * l, coords[i+1][2]]
+
+        v1 = list(map(lambda x, y: x - y, tl, bl))
+        v2 = list(map(lambda x, y: x - y, tr, tl))
+        normal = np.cross(v1, v2)
+        normal = normal / np.linalg.norm(normal) if settings['normalize_manual'] else normal
+        normals.append(normal)
+    return normals
+
+
+def get_normals_vertexes_per_base(normals_l, normal_base):
+    n_top_all = []
+    for i in range(len(normals_l)):
+        n1 = list(map(lambda x, y, z: x + y + z, normal_base, normals_l[i], normals_l[0])) if i == len(
+            normals_l)-1 else list(map(lambda x, y, z: x + y + z, normal_base, normals_l[i], normals_l[i+1]))
+
+        n1 = n1 / np.linalg.norm(n1) if settings['normalize_manual'] else n1
+        n_top_all.append(n1)
+    return n_top_all
+
+
+def get_all_normals_vertexes(normals_l, coords):
+    normal_top = [0, 1, 0]
+    normal_btm = [0, -1, 0]
+
+    n_tops = get_normals_vertexes_per_base(normals_l, normal_top)
+    n_btms = get_normals_vertexes_per_base(normals_l, normal_btm)
+
+    return n_tops, n_btms
+
+
+def draw_normals(coords, l):
+    normals = get_normals_lateral(coords, l)
+    n_top, n_btm = get_all_normals_vertexes(normals, coords)
+    coords_0 = coords.pop(0)
+    coords_new = coords
+    coords_new.append(coords_0)
+    glColor3f(1, 1, 1)
+    if settings['display_normals']:
+        glBegin(GL_LINES)
+        for i in range(len(normals)):
+            glVertex3f(coords_new[i][0], coords_new[i][1], coords_new[i][2])
+            glVertex3f(coords_new[i][0]+n_btm[i][0], coords_new[i]
+                    [1]+n_btm[i][1], coords_new[i][2]+n_btm[i][2])
+
+            glVertex3f(coords_new[i][0], coords_new[i][1] + 2*l, coords_new[i][2])
+            glVertex3f(coords_new[i][0]+n_top[i][0], coords_new[i]
+                    [1] + 2*l + n_top[i][1], coords_new[i][2]+n_top[i][2])
+        glEnd()
+    return coords_new, n_top, n_btm
+
+
+def draw_prism():
+    l = 3
+    coords = [[0, 0, 0],
+              [l, 0, 0],
+              [3/2*l, 0, np.sqrt(3)/2*l],
+              [l, 0, np.sqrt(3)*l],
+              [0, 0, np.sqrt(3)*l],
+              [-1/2*l, 0, np.sqrt(3)/2*l]]
+    glFrontFace(GL_CW)
+    glColor3f(1, 0, 0)
+
+    if settings['display_bases']:
+        glBegin(GL_LINE_LOOP)
+        for coord in coords:
+            glVertex3f(coord[0], coord[1], coord[2])
+        glEnd()
+
+        glBegin(GL_LINE_LOOP)
+        for coord in coords:
+            glVertex3f(coord[0], coord[1] + l * 2, coord[2])
+        glEnd()
+
+    coords_new, n_top, n_btm = draw_normals(coords, l)
+    
+    glBegin(GL_QUADS)
+    for i in range(len(coords_new)):
+        glNormal3f(*n_btm[i])
+        glVertex3f(*coords_new[i])
+
+        glNormal3f(*n_top[i])
+        glVertex3f(coords_new[i][0], coords_new[i][1] + 2 * l, coords_new[i][2])
+
+        if i != len(coords_new) - 1:
+            glNormal3f(*n_top[i+1])
+            glVertex3f(coords_new[i+1][0], coords_new[i+1][1] + 2 * l, coords_new[i+1][2])
+
+            glNormal3f(*n_btm[i+1])
+            glVertex3f(coords_new[i+1][0], coords_new[i+1][1], coords_new[i+1][2])
+        else:
+            glNormal3f(*n_top[0])
+            glVertex3f(coords_new[0][0], coords_new[0][1] + 2 * l, coords_new[0][2])
+
+            glNormal3f(*n_btm[0])
+            glVertex3f(coords_new[0][0], coords_new[0][1], coords_new[0][2])
+
+    glEnd()
+
+
+def draw_light_src(l):
+    l0 = [2*l, -l, 0]
+    l1 = [0, 3*l, 2*l]
+    mtClr0 = [1, 0.4, 0.2] # Цвет материала
+    lghtClr0 = [1, 1, 0] # Цвет источника света 0
+    lghtClr1 = [1, 0, 1] # Цвет источника света 1
+
+    glPointSize(10)
+    glEnable(GL_POINT_SMOOTH)
+    glBegin(GL_POINTS)
+
+    glColor3f(*lghtClr0)
+    glVertex3f(*l0)
+
+    glColor3f(*lghtClr1)
+    glVertex3f(*l1)
+
+    glEnd()
+    glEnable(GL_LIGHTING) if settings['lightning_active'] else glDisable(GL_LIGHTING)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, (GLfloat * 4)(*mtClr0, 0))
+    #if settings['diffuse']:
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, (GLfloat * 4)(*mtClr0, 0))
+    glLightfv(GL_LIGHT0, GL_POSITION, (GLfloat * 4)(*l0, 0))
+    glLightfv(GL_LIGHT0, GL_SPECULAR, (GLfloat * 4)(*lghtClr0, 0))
+
+    glLightfv(GL_LIGHT1, GL_POSITION, (GLfloat * 4)(*l1, 0))
+    glLightfv(GL_LIGHT1, GL_SPECULAR, (GLfloat * 4)(*lghtClr1, 0))
+
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHT1) if settings['static_source_active'] else glDisable(GL_LIGHT1)
+
+
+
+@window.event
+def on_draw():
+    window.clear()
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-w, w, -w, w, -w, w)
+    glRotatef(-230, 0, 1, 0)
+    glRotatef(20, 1, 0, 0)
+
+    glDisable(GL_LIGHTING)
+    glEnable(GL_DEPTH_TEST) if settings['depth_test'] else glDisable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE) if settings['cull_face'] else glDisable(GL_CULL_FACE)
+    glEnable(GL_NORMALIZE) if settings['normalize_auto'] else glDisable(GL_NORMALIZE)
+    show_axis()
+    draw_light_src(3)
+    draw_prism()
+
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    global settings
+    if symbol == key._1: 
+        settings['display_bases'] = not settings['display_bases']
+    elif symbol == key._3:
+        settings['display_normals'] = not settings['display_normals']
+    elif symbol == key._6:
+        settings['depth_test'] = not settings['depth_test']
+    elif symbol == key._7:
+        settings['cull_face'] = not settings['cull_face']
+    elif symbol == key._8:
+        settings['static_source_active'] = not settings['static_source_active']
+    elif symbol == key._9:
+        settings['normalize_manual'] = not settings['normalize_manual']
+    elif symbol == key._0:
+        settings['normalize_auto'] = not settings['normalize_auto']
+    elif symbol == key.MINUS:
+        settings['lightning_active'] = not settings['lightning_active']
+    elif symbol == key.EQUAL:
+        settings['diffuse'] = not settings['diffuse']
+        
+
+
+app.run()
